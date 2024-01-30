1fa38574ec2dc2848b5970151f0ed58de9bae46e
Priyantha Gunasekara <priyantha@wired2perform.com>
2024-01-30 10:41:21 +0530

    initialize project

---
+++ src/main/java/com/clab/tbaseauth/TBaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.clab.tbaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBaseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBaseAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clab/tbaseauth/TBaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clab.tbaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBaseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
